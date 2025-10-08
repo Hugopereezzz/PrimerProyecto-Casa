2e3f276ba1a667173e5010d9b0a8f727c4897c42
Hugopereezzz <hugopereezzzz@gmail.com>
2025-10-08 11:28:45 +0200

    añado carrera y sprites

--- src/app/components/carrera/carrera.ts
+++ src/app/components/carrera/carrera.ts
@@ -1,8 +1,9 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carrera',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carrera.html',
   styleUrl: './carrera.css'
 })
