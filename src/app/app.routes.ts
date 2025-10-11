ae63978ffac5339612fe7d0e1ed4b3f45f6f0a51
Hugopereezzz <hugopereezzzz@gmail.com>
2025-10-12 00:34:01 +0200

    tarjetas personajes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Contador } from './components/contador/contador';
 import { Matatopos } from './components/matatopos/matatopos';
 import { Carrera } from './components/carrera/carrera';
 import { PasoParametros } from './components/paso-parametros/paso-parametros';
+import { ListaPersonajes } from './components/lista-personajes/lista-personajes';
 
 export const routes: Routes = [
     { path: 'contador', component: Contador},
     { path: 'matatopos', component: Matatopos},
     { path: 'carrera', component: Carrera},
-    { path: 'pasoparametros', component: PasoParametros},
-
+    { path: 'paso-parametros', component: PasoParametros},
+    { path: 'lista-personajes', component: ListaPersonajes},
 ];
